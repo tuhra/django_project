50ff46380ab758f2196a357998604194add9774f
thura <aungthura.win@umgroups.com>
2022-06-03 10:13:51 +0630

    Part 5 Database & Migration

--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-	{
-		'author': 'Thura',
-		'title': 'Lorem Ipsum One',
-		'content': 'Blog Content One',
-		'date_posted': 'Jun 2, 2022'
-	},
-	{
-		'author': 'Sandi',
-		'title': 'Lorem Ipsum Two',
-		'content': 'Blog Content Two',
-		'date_posted': 'Jun 2, 2022'
-	},
-	{
-		'author': 'Thura',
-		'title': 'Lorem Ipsum Three',
-		'content': 'Blog Content Three',
-		'date_posted': 'Jun 2, 2022'
-	},
-]
+from .models import Post
 
 def home(request):
 	context = {
-		'posts': posts
+		'posts': Post.objects.all()
 	}
 	return render(request, 'blog/home.html', context)
 
